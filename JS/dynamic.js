44da2115810ea85e6882fba83f1e2330a32cb628
omar <omarabdelmoaty816@gmail.com>
2023-08-06 04:37:50 +0300

    Add new JS code

--- JS/dynamic.js
+++ JS/dynamic.js
@@ -54,3 +54,101 @@ foodType.forEach(ele => {
 })
 
 // -------------------------------------------------- // 
+
+// Gallary Section : Img slider
+let imgs = Array.from(document.querySelectorAll(".gallery .img-gallery img"));
+let ulBullets = document.querySelector(".img-controls .bullets ul");
+let nextBtn = document.querySelector(".img-controls .next");
+let prevBtn = document.querySelector(".img-controls .prev");
+
+let numOfSlides = imgs.length;
+let currentSlide = 1;
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+/* Create Bullets */
+for (let i = 1; i <= numOfSlides; i++) {
+    let bulletItem = document.createElement("li");
+    bulletItem.className = "bullet-item";
+    bulletItem.setAttribute("index-bullet", i);
+    bulletItem.appendChild(document.createTextNode(i));
+    ulBullets.appendChild(bulletItem);
+};
+
+let ulBulletsItems = Array.from(document.querySelectorAll(".img-controls .bullets ul .bullet-item"));
+
+/* Click on button to change slide */
+ulBulletsItems.forEach(ele => {
+    ele.addEventListener("click", () => {
+        currentSlide = parseInt(ele.getAttribute("index-bullet"));
+        addActive()
+    })
+})
+
+addActive();
+
+/* Add class active to current bullet */
+function addActive() {
+
+    ulBulletsItems.forEach(bullet => {
+        bullet.classList.remove("active");
+        if (bullet.getAttribute("index-bullet") == currentSlide) {
+            bullet.classList.add("active");
+        }
+    })
+
+    imgs.forEach(img => {
+        img.classList.remove("active");
+        if (img.dataset.index == currentSlide) {
+            img.classList.add("active");
+        }
+    })
+
+    if (currentSlide == numOfSlides) {
+        nextBtn.classList.add("disable");
+    } else {
+        nextBtn.classList.remove("disable")
+    }
+
+    if (currentSlide == 1) {
+        prevBtn.classList.add("disable");
+    } else {
+        prevBtn.classList.remove("disable")
+    }
+    
+}
+
+
+/* Change the slide from next & previous buttons */
+function nextSlide() {
+    if (nextBtn.classList.contains("disable")) {
+        return false;
+    } else {
+        currentSlide++;
+        addActive();
+    }
+}
+
+function prevSlide() {
+    if (prevBtn.classList.contains("disable")) {
+        return false;
+    } else {
+        currentSlide--;
+        addActive();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
